e9c0b4739a9ddbe820b6bbf0156d46689872444a
Millenmark <millenmark.dev@gmail.com>
2024-03-20 11:13:06 +0800

    update: added product routes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,14 +1,10 @@
 import { Router } from "express";
 
 /** IMPORT: CONTROLLER */
-import {
-  showAllProducts,
-  showAllProductsStatic,
-} from "../controllers/productController.js";
+import { showAllProducts } from "../controllers/productController.js";
 
 const router = Router();
 
 router.route("/").get(showAllProducts);
-router.route("/static").get(showAllProductsStatic);
 
 export default router;
