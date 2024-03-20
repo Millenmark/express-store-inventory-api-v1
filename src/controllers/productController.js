2434966a4c3f8755736d12984fe448688c17de69
Millenmark <millenmark.dev@gmail.com>
2024-03-20 11:12:45 +0800

    update: added product controller (showAllProducts)

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,8 +1,56 @@
-export const showAllProducts = async (req, res) => {
-  throw new Error("Testing Error");
-  res.status(200).json({ message: "Testing this route..." });
-};
+/** IMPORT: MODEL */
+import Product from "../models/Product.js";
 
-export const showAllProductsStatic = async (req, res) => {
-  res.status(200).json({ message: "try lang" });
+export const showAllProducts = async (req, res) => {
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObject = {};
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 5;
+  const skip = (page - 1) * limit;
+
+  if (featured) queryObject.featured = featured === "true" ? true : false;
+  if (company) queryObject.company = company;
+  if (name) queryObject.name = { $regex: name, $options: "i" };
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    const options = ["price", "rating"];
+
+    const filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    filters.split(",").forEach((item) => {
+      const [field, ...rest] = item.split("-");
+      // console.log(rest); // [ '$gte', '40', '$lte', '45' ]
+
+      if (options.includes(field)) {
+        const obj = rest.reduce((acc, curr, index) => {
+          if (index % 2 === 0) {
+            acc[curr] = Number(rest[index + 1]);
+          }
+          return acc;
+        }, {});
+
+        queryObject[field] = obj;
+      }
+    });
+  }
+
+  let result = Product.find(queryObject).skip(skip).limit(limit);
+
+  if (sort) result = result.sort(sort.split(",").join(" "));
+  else result = result.sort("createdAt");
+  if (fields) result.select(fields.split(",").join(" "));
+
+  const products = await result;
+
+  res.status(200).json({ products });
 };
